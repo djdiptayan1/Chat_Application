5600d2a93826b5032ca1aa2289cc2e91858c3e5a
SrijitK10 <120118631+SrijitK10@users.noreply.github.com>
2023-10-09 21:31:44 +0530

    Add files via upload
    Updated chatserver file with mySQL connectivity and database for user credentials

---
+++ GUI-test/src/ChatServer2.java
@@ -0,0 +1,143 @@
+import java.io.*;
+import java.net.*;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ChatServer2 {
+    private static final int PORT = 3000;
+    private static Map<PrintWriter, String> clientMap = new HashMap<>();
+    private static List<String> chatHistory = new ArrayList<>(); // List to store chat history
+    private static Connection dbConnection; // Database connection
+
+    public static void main(String[] args) {
+        // Establish a database connection
+        establishDBConnection();
+
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Chat server is running...");
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Client connected: " + clientSocket);
+
+                Thread clientThread = new ClientHandler(clientSocket);
+                clientThread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // Close the database connection when the server is shut down
+            closeDBConnection();
+        }
+    }
+
+    private static void establishDBConnection() {
+        try {
+            // Replace the database URL, username, and password with your own
+            String dbUrl = "jdbc:mysql://localhost:3306/chat_database";
+            String dbUser = "root";
+            String dbPassword = "srijit";
+            dbConnection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+            System.out.println("Database connection established.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void closeDBConnection() {
+        try {
+            if (dbConnection != null && !dbConnection.isClosed()) {
+                dbConnection.close();
+                System.out.println("Database connection closed.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class ClientHandler extends Thread {
+        private Socket socket;
+        private PrintWriter writer;
+        private String username;
+
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        public void run() {
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                writer = new PrintWriter(socket.getOutputStream(), true);
+
+                String username = reader.readLine();
+                String password = reader.readLine();
+
+                if (authenticateUser(username, password)) {
+                    writer.println("AUTHENTICATED");
+                    this.username = username;
+
+                    synchronized (clientMap) {
+                        clientMap.put(writer, username);
+                    }
+
+                    // Send chat history to the client
+                    for (String historyMessage : chatHistory) {
+                        writer.println(historyMessage);
+                    }
+
+                    String message;
+                    while ((message = reader.readLine()) != null) {
+                        String formattedMessage = username + ": " + message;
+                        System.out.println("Received: " + formattedMessage);
+                        chatHistory.add(formattedMessage); // Store the message in chat history
+                        broadcast(formattedMessage);
+                    }
+                } else {
+                    writer.println("AUTH_FAILED");
+                    socket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                synchronized (clientMap) {
+                    clientMap.remove(writer);
+                }
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        private void broadcast(String message) {
+            synchronized (clientMap) {
+                for (PrintWriter writer : clientMap.keySet()) {
+                    writer.println(message);
+                }
+            }
+        }
+
+        private boolean authenticateUser(String username, String password) {
+            // Use the database to authenticate users
+            try {
+                String query = "SELECT password FROM users WHERE username = ?";
+                PreparedStatement preparedStatement = dbConnection.prepareStatement(query);
+                preparedStatement.setString(1, username);
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                if (resultSet.next()) {
+                    String storedPassword = resultSet.getString("password");
+                    return password.equals(storedPassword);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            return false;
+        }
+    }
+}
---
+++ GUI-test/src/DatabaseSetup.java
@@ -0,0 +1,59 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseSetup {
+    // Database connection parameters
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/";
+    private static final String DB_USER = "root"; // Replace with your MySQL username
+    private static final String DB_PASSWORD = "srijit"; // Replace with your MySQL password
+    private static final String DB_NAME = "chat_database"; // Modify the database name if needed
+
+    public static void main(String[] args) {
+        try {
+            // Create a database connection
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+            // Create a new database
+            createDatabase(connection);
+
+            // Create a table to store user credentials
+            createUsersTable(connection);
+
+            // Close the connection
+            connection.close();
+
+            System.out.println("Database setup complete.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createDatabase(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+
+        // Create the database if it doesn't exist
+        String createDBSQL = "CREATE DATABASE IF NOT EXISTS " + DB_NAME;
+        statement.executeUpdate(createDBSQL);
+
+        statement.close();
+    }
+
+    private static void createUsersTable(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+
+        // Use the newly created database
+        String useDBSQL = "USE " + DB_NAME;
+        statement.executeUpdate(useDBSQL);
+
+        // Create a table to store user credentials
+        String createUserTableSQL = "CREATE TABLE IF NOT EXISTS users (" +
+                "id INT AUTO_INCREMENT PRIMARY KEY," +
+                "username VARCHAR(255) NOT NULL," +
+                "password VARCHAR(255) NOT NULL)";
+        statement.executeUpdate(createUserTableSQL);
+
+        statement.close();
+    }
+}
